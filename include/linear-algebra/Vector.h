18816bfb3c4cb7710fa62dd96e56928f88ab11e0
Dan Morris <dm986@bath.ac.uk>
2021-04-06 17:59:59 +0100

    Added Vector class

---
+++ include/linear-algebra/Vector.h
@@ -0,0 +1,187 @@
+#pragma once
+
+#include <vector>
+#include <array>
+
+// Vec of type T and size S
+template<typename T, unsigned int S>
+class Vec {
+private:
+    // Array containing the data values
+    std::array<T, S> data;
+public:
+    // Default constructor. Initialises all data to 0;
+    Vec() {
+        for (int i = 0; i < S; i++) data[i] = 0;
+    };
+
+    // Copy Constructor from array
+    Vec(std::array<T, S> &data) : data(data) {}
+
+    // Move Constructor of an array
+    Vec(std::array<T, S> &&data) : data(data) {}
+
+    // Initialises a vector from the given values. Fills up to the length of the list or the vector and the rest will be 0.
+    Vec(std::initializer_list<T> args) {
+        int cursor = 0;
+        for (auto i = args.begin(); i != args.end() && cursor < S; i++) {
+            data[cursor++] = *i;
+        }
+
+        while (cursor < S) {
+            data[cursor++] = 0;
+        }
+    }
+
+    // Mutable accessor
+    T &operator[](unsigned int i) {
+        return data[i];
+    }
+
+    // Immutable accessor
+    const T &operator[](unsigned int i) const {
+        return data[i];
+    }
+
+    // Tests is 2 vectors are equal
+    bool equals(const Vec &b) const {
+        for (int i = 0; i < S; i++) if ((*this)[i] != b[i]) return false;
+        return true;
+    }
+
+    // Operator overload for vector equality
+    bool operator==(const Vec &b) const {
+        return (*this).equals(b);
+    }
+
+    // Operator overload for vector inequality
+    bool operator!=(const Vec &b) const {
+        return !(*this).equals(b);
+    }
+
+    // Returns the piecewise sum result of 2 vectors
+    Vec plus(const Vec &b) const {
+        Vec sum;
+        for (int i = 0; i < S; i++) sum[i] = (*this)[i] + b[i];
+        return sum;
+    }
+
+    // Operator Overload for vector addition
+    Vec operator+(const Vec &b) const {
+        return (*this).plus(b);
+    }
+
+    // Operator overload for vector addition-assignment
+    void operator+=(const Vec &b) {
+        (*this) = (*this).plus(b);
+    }
+
+    // Returns the piecewise difference of 2 vectors
+    Vec minus(const Vec &b) const {
+        Vec difference;
+        for (int i = 0; i < S; i++) difference[i] = (*this)[i] - b[i];
+        return difference;
+    }
+
+    // Operator overload for vector subtraction
+    Vec operator-(const Vec &b) const {
+        return (*this).minus(b);
+    }
+
+    // Operator overload for vector subtraction-assignment
+    void operator-=(const Vec &b) {
+        (*this) = (*this).minus(b);
+    }
+
+    // Scales a vector by a given constant
+    Vec scale(T m) const {
+        Vec scaled;
+        for (int i = 0; i < S; i++) scaled[i] = m * (*this)[i];
+        return scaled;
+    }
+
+    // Operator overload for constant multiplication
+    Vec operator*(const T &b) const {
+        return (*this).scale(b);
+    }
+
+    // Operator overload for constant multiplication-assignment
+    void operator*=(const T &b) {
+        (*this) = (*this).scale(b);
+    }
+
+    // Normalises the vector
+    void normalise() {
+        (*this) = (*this).normalised();
+    }
+
+    // Returns a normalised copy of the vector
+    Vec normalised() const {
+        return (*this).scale(1.0 / (*this).length());
+    }
+
+    // Returns the dot product of 2 vectors
+    T dot_product(const Vec &b) {
+        T accumulator = 0;
+        for (int i = 0; i < S; i++) accumulator += (*this)[i] * b[i];
+        return accumulator;
+    }
+
+    // Returns the cross product of two vectors of length 3
+    Vec<T, 3> cross_product(const Vec<T, 3> &b) {
+        Vec<T, 3> cross;
+        cross[0] = (*this)[1] * b[2] - (*this)[2] * b[1];
+        cross[1] = (*this)[2] * b[0] - (*this)[0] * b[2];
+        cross[2] = (*this)[0] * b[1] - (*this)[1] * b[0];
+        return cross;
+    }
+
+    // Returns the magnitude of the vector
+    double length() const {
+        double accumulator = 0;
+        for (int i = 0; i < S; i++) accumulator += (double) data[i] * (double) data[i];
+        return sqrt(accumulator);
+    }
+
+    // Returns the size of the angle between 2 vectors in radians
+    double angle_between(const Vec &b) {
+        double length_product = (*this).length() * b.length();
+        double dot_prod = (double) (*this).dot_product(b);
+        return acos(dot_prod / length_product);
+    }
+
+    // Method for concatenating 2 vectors
+    template<unsigned int S2>
+    Vec<T, S + S2> concat(const Vec<T, S2> &b) {
+        Vec<T, S + S2> concat;
+        for (int i = 0; i < S + S2; i++) {
+            if (i < S) concat[i] = (*this)[i];
+            else concat[i] = b[i - S];
+        }
+        return concat;
+    }
+
+    // Method for appends a value to the end of a vector
+    Vec<T, S + 1> append(T v) {
+        Vec<T, S + 1> concat;
+
+        for (int i = 0; i < S; i++) concat[i] = (*this)[i];
+        concat[S] = v;
+
+        return concat;
+    }
+
+    // Splits a vector into 2 at the given index
+    template<unsigned int SPLIT_IDX>
+    std::tuple<Vec<T, SPLIT_IDX>, Vec<T, S - SPLIT_IDX>> split() {
+        Vec<T, SPLIT_IDX> a;
+        Vec<T, S - SPLIT_IDX> b;
+
+        for (int i = 0; i < S; i++) {
+            if (i < SPLIT_IDX) a[i] = (*this)[i];
+            else b[i - SPLIT_IDX] = (*this)[i];
+        }
+
+        return std::make_tuple(a, b);
+    }
+};
---
+++ src/test.cpp
@@ -0,0 +1,107 @@
+#include <linear-algebra/Vector.h>
+
+#include <iostream>
+
+#define TEST(function) \
+    if (function()) { \
+        std::cout << "TEST: [" << #function << "] SUCCESS" << std::endl; \
+    } else { \
+        std::cout << "TEST: [" << #function << "] FAILED" << std::endl;\
+    }
+
+#define TEST_ASSERT(x) if (!(x)) return false
+#define TEST_COMPLETE return true
+
+namespace Test {
+    bool Vector_constructor_test() {
+        Vec<int, 6> v{0, 4, 5, 6};
+        TEST_ASSERT(v[0] == 0);
+        TEST_ASSERT(v[1] == 4);
+        TEST_ASSERT(v[2] == 5);
+        TEST_ASSERT(v[3] == 6);
+        TEST_ASSERT(v[4] == 0);
+        TEST_ASSERT(v[5] == 0);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_sum() {
+        Vec<double, 3> a{3.5, 0.0, 11.7};
+        Vec<double, 3> b{5.5, 8.0, 8.3};
+        Vec<double, 3> c = a + b;
+        Vec<double, 3> d{9.0, 8.0, 20.0};
+        TEST_ASSERT(c == d);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_concat() {
+        Vec<int, 3> v1{1, 2, 3};
+        Vec<int, 4> v2 = v1.append(4);
+        Vec<int, 2> v3{4, 5};
+        Vec<int, 5> v4 = v1.concat(v3);
+        TEST_ASSERT(v2[0] == 1);
+        TEST_ASSERT(v2[1] == 2);
+        TEST_ASSERT(v2[2] == 3);
+        TEST_ASSERT(v2[3] == 4);
+        TEST_ASSERT(v4[0] == 1);
+        TEST_ASSERT(v4[1] == 2);
+        TEST_ASSERT(v4[2] == 3);
+        TEST_ASSERT(v4[3] == 4);
+        TEST_ASSERT(v4[4] == 5);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_difference() {
+        Vec<double, 3> a{3.5, 0.0, 11.0};
+        Vec<double, 3> b{5.5, 8.0, 8.0};
+        Vec<double, 3> c = a - b;
+        Vec<double, 3> d{-2.0, -8.0, 3.0};
+        TEST_ASSERT(c == d);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_split() {
+        Vec<int, 5> v{5, 8, 2, 3, 6};
+        auto[a, b] = v.split<2>();
+        TEST_ASSERT(a[0] == 5);
+        TEST_ASSERT(a[1] == 8);
+        TEST_ASSERT(b[0] == 2);
+        TEST_ASSERT(b[1] == 3);
+        TEST_ASSERT(b[2] == 6);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_length() {
+        Vec<int, 3> v{3, 4};
+        TEST_ASSERT(v.length() == 5.0);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_angle_between() {
+        Vec<int, 2> a{1, 0};
+        Vec<int, 2> b{0, 1};
+        double angle = a.angle_between(b);
+        TEST_ASSERT(angle == M_PI_2);
+        TEST_COMPLETE;
+    }
+
+    bool Vector_normalisation() {
+        Vec<double, 3> a{6, 2, 5};
+        double normal_length = a.normalised().length();
+        TEST_ASSERT(normal_length == 1.0);
+        a.normalise();
+        TEST_ASSERT(a.length() == 1.0);
+        TEST_COMPLETE;
+    }
+}
+
+int main() {
+    using namespace Test;
+    TEST(Vector_constructor_test)
+    TEST(Vector_sum)
+    TEST(Vector_difference)
+    TEST(Vector_split)
+    TEST(Vector_concat)
+    TEST(Vector_length)
+    TEST(Vector_angle_between)
+    TEST(Vector_normalisation)
+}
