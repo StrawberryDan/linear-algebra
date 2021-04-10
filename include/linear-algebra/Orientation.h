511f636cf0383d1dbf1936f64c122bb99d114eee
Dan Morris <dm986@bath.ac.uk>
2021-04-10 15:32:51 +0100

    Removed typedef double Angle;

--- include/linear-algebra/Orientation.h
+++ include/linear-algebra/Orientation.h
@@ -1,17 +1,14 @@
 #pragma once
 
 namespace LinearAlgebra::Orientation {
-    // Typedef mapping doubles to Angles for readability
-    typedef double Angle;
-
     // Structure representing an orientation in 3D with Euler Angles.
     struct EulerAngle {
-        Angle x, y, z;
+        double x, y, z;
 
         // Default zero constructor
         EulerAngle() : x(0.0), y(0.0), z(0.0) {}
         // Explicit value constructor
-        EulerAngle(Angle x, Angle y, Angle z) : x(x), y(y), z(z) {}
+        EulerAngle(double x, double y, double z) : x(x), y(y), z(z) {}
 
         // Pitch accessor
         constexpr double &pitch() { return x; }
